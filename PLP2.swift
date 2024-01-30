abd4360d18911dce2c8c6e7515f4ff3ef31f3f81
Karley <156860147+klafave1@users.noreply.github.com>
2024-01-30 00:23:27 -0500

    Add files via upload

---
+++ PLP2.swift
@@ -0,0 +1,24 @@
+import UIKit
+
+import UIKit
+
+// int
+var integerVal: Int = 10
+
+// string
+var stringVal: String = "PLP2"
+
+// floating-point number
+var floatVal: Float = 3.14
+
+// double
+var doubleVal: Double = 3.14159
+
+// boolean
+var booleanVal: Bool = true
+
+// array
+var arrayVal: [Int] = [1, 2, 3, 4, 5]
+
+// dictionary
+var dictionaryVal: [String: Any] = ["Name": "Karley", "Age": 20, "simmonsStudent": true]
